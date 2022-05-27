abb2ec93398f1e8ff2f129d96e7889771dd519b0
Renan de Assis Alves <renan.assis.alves@hotmail.com>
2022-05-27 17:22:24 -0300

    Adicionadas seções 2 e 3

--- secao02/main.py
+++ secao02/main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get('/')
 async def raiz():
     return {"msg" : "FastAPI na Geek University"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
---
+++ secao03/main.py
@@ -0,0 +1,109 @@
+from typing import Dict, List, Optional, Any
+
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+from fastapi import Response
+from fastapi import Path
+from fastapi import Query
+from fastapi import Header
+from fastapi import Depends
+
+from fastapi.responses import JSONResponse
+
+from time import sleep
+
+from models import Curso
+
+
+def fake_db():
+    try:
+        print("Abrindo conexão com o banco de dados...")
+        sleep(1)
+    finally:
+        print("Fechando conexão com o banco de dados...")
+        sleep(1)
+
+
+app = FastAPI(
+    title="Api de Cursos da Geek University",
+    version="0.0.1",
+    description="Uma api para estudo do FastAPI"
+)
+
+cursos = {
+    1: {
+        "titulo": "Programação para Leigos",
+        "aulas" : 112,
+        "horas" : 58
+    },
+    2: {
+        "titulo": "Algoritmos e Lógica de Programação",
+        "aulas" : 87,
+        "horas" : 67
+    }
+}
+
+@app.get('/cursos',
+         description="Retorna todos os cursos ou uma lista vazia.",
+         summary="Retorna todos os cursos",
+         response_model=Dict[int, Curso]
+         )
+async def get_cursos(db: Any = Depends(fake_db)):
+    return cursos
+
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int = Path(default=None, title="ID do curso", description="Deve ser entre 1 e 2", gt=0, lt=3), db: Any = Depends(fake_db)):
+    try:
+        curso = cursos[curso_id]
+        return curso
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado."
+        )
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
+    next_id: int = len(cursos) + 1
+    curso.id = next_id
+    cursos[next_id] = curso
+    return curso
+
+
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        curso.id = curso_id
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com o id {curso_id}")
+
+
+@app.delete('/cursos/{curso_id}')
+async def del_curso(curso_id: int, db: Any = Depends(fake_db)):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com o id {curso_id}")
+
+
+@app.get('/calculadora')
+async def calcular(a: int = Query(default=None, gt=5),
+                   b: int = Query(default=None, gt=10),
+                   x_geek: str = Header(default=None),
+                   c: Optional[int] = None):
+    soma = a + b
+    if c:
+        soma = soma + c
+
+        print(f'X_Geek: {x_geek}')
+
+    return {"resultado" : soma}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True,
+                reload=True)
